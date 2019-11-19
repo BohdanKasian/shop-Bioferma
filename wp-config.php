b3779faf32f10b2f5d9b568dddfaad595078ce83
BohdanKasian <psaiker12@gmail.com>
2019-11-19 20:06:29 +0200

    her

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'root' );
 define( 'DB_PASSWORD', '' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'mybioferma.com' );
+define( 'DB_HOST', 'https://bioferma.herokuapp.com' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
